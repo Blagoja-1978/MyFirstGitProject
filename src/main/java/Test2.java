b9a392167f443919d9cd116b3173c949847a25a8
Blagoja <blagojaan@yahoo.com>
2025-05-06 20:30:19 +0200

    added: Test3.java

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,6 +1,7 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("*2. Test wurde geschrieben");
+        System.out.println("*3. Test wurde geschrieben");
 
     }
 }
