8276a609a83f9c83778d92cc7f161e2daae4e0bd
Blagoja <blagojaan@yahoo.com>
2025-05-06 21:12:57 +0200

    Test3 wurde korregiert
     added: 4. Test

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,7 +1,8 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("*2. Test wurde geschrieben");
-        System.out.println("*3. Test wurde geschrieben");
+        System.out.println("*3. Test wurde geschrieben aber es gab falsche kode deswegen habe ich das korregiert.");
+        System.out.println("4. Test habe ich auch geschrieben");
 
     }
 }
